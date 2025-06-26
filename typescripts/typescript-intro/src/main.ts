728d0c2148168c0d7e8c674c4d3d87f1ecb60c1d
alejandro <alejandro.b.venegas.ch@gmail.com>
2025-06-26 12:33:15 -0400

    primero

---
+++ typescripts/typescript-intro/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+// import './topics/1-basic-types';
+import './topics/2-object-interface';
+
+const  app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `hola mundo `;
